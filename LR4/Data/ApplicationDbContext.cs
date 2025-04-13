9006976362c75dde855c261b24c0b6cd762bf09a
olegzig <boris37544@gmail.com>
2025-04-13 19:56:19 +0300

    lr4 init

---
+++ LR4/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using LR4.Models;
+
+namespace LR4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<LR4.Models.MenuItem> MenuItem { get; set; } = default!;
+    }
+}
