f038d8416fc438528beac69e41921fa290b59805
olegzig <boris37544@gmail.com>
2025-04-13 19:43:38 +0300

    lr3 done

---
+++ LR3/Views/Shared/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LR3.Views.Shared.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
