cafa5e349b143de4491ae1c1e6b0dd8f15701936
olegzig <boris37544@gmail.com>
2025-04-13 15:15:32 +0300

    Lr2 done

--- LR2/Controllers/HomeController.cs
+++ LR2/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using LR2.Models;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 using System.Diagnostics;
 
@@ -8,16 +9,30 @@ namespace LR2.Controllers
 {
     public class HomeController : Controller
     {
+        private List<ListDemo> _listDemo;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _listDemo = new List<ListDemo> {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"}, 
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"} 
+            };
         }
 
         public IActionResult Index()
         {
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+            ViewData["Text"] = "Лабараторная работа 2";
             return View();
         }
+    }
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
     }
 }
